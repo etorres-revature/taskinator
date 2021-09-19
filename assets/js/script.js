c561453485de55145a89a4c7479cb80bf9907a0c
Eric D. Torres <etorresnotary@gmail.com>
2021-09-19 14:02:29 -0500

    createTaskActions func to add edit and delete buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,24 +10,24 @@ const taskFormHandler = (event) => {
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
   if (!taskNameInput || !taskTypeInput) {
-      alert("Please fill the form out in its entirety!!")
-      return false;
+    alert("Please fill the form out in its entirety!!");
+    return false;
   }
 
   //task data in object form
   var taskDataObj = {
-      name: taskNameInput,
-      type: taskTypeInput
+    name: taskNameInput,
+    type: taskTypeInput,
   };
 
   //send it as an argument to createTaskEl
-  createTaskEl(taskDataObj)
+  createTaskEl(taskDataObj);
 
   formEl.reset();
 };
 
 const createTaskEl = (taskDataObj) => {
-      //create li with styling
+  //create li with styling
   let listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
 
@@ -52,7 +52,28 @@ const createTaskEl = (taskDataObj) => {
   tasksToDoEl.append(listItemEl);
 
   // increase task counter for next unique id
-  taskIDCounter++
-}
+  taskIDCounter++;
+};
+
+const createTaskActions = (taskID) => {
+  let actionContainerEl = document.createElement("div");
+  actionContainerEl.className = "task-actions";
+
+  // create edit button
+  const editButtonEl = document.createElement("button");
+  editButtonEl.textContent = "Edit";
+  editButtonEl.className = "btn edit-btn";
+  editButtonEl.setAttribute("data-task-id", taskID);
+
+  actionContainerEl.appendChild(editButtonEl);
+
+  // create delete button
+  const deleteButtonEl = document.createElement("button");
+  deleteButtonEl.textContent = "Delete";
+  deleteButtonEl.className = "btn delete-btn";
+  deleteButtonEl.setAttribute("data-task-id", taskID);
+
+  actionContainerEl.appendChild(deleteButtonEl);
+};
 
 formEl.addEventListener("submit", taskFormHandler);
